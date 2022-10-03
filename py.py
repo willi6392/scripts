9c89aa5a7dca31bfa2a0385ff06971df6fd6bec5
Willi <83806444+JusteWilli@users.noreply.github.com>
2022-10-03 20:55:10 +0200

    Add files via upload

---
+++ py.py
@@ -0,0 +1,10 @@
+a = ["freshman", "freshman Schüler / -in im ersten Jahr an der High School", "stand", "Stand", "cheerleader", "Cheerleader", "drama group", "Theatergruppe", "role", "Rolle", "to perform", "aufführen, spielen", "play", "Theaterstück, Schauspiel", "practice", "Training , Übung", "tough", "hart, rau", "to earn", "verdienen", "talent scout", "Talentscout, Talentsucher", "photographer", "Fotograf", "Annual Production", "Annual Production; Fach, in dem der Jahresbericht/das Jahresbericht erstellt wird", "trash can", "Mülltonne", "rumour", "Gerücht", "erthquake", "Erdbeben", "unhurt", "unverletzt", "squad", "Gruppe, Team", "altogether", "insgesamt", "cheer", "Jubelschrei, Beifallsruf", "strength", "Stärke, Kraft", "stereotype", "Vorurteil", "to clap", "klatschen", "thin", "dünn", "skill", "Können, Fähigkeit", "color", "Farbe", "determination", "Wille, Entschlossenheit", "stunt", "Kunststück", "athletic", "athletisch, sportlich", "safety guidelines", "Sicherheitsrichtlinien", "embarrassing", "peinlich", "locker", "Schließfach", "to block off", "verriegeln", "gate", "Tor", "spikee", "Spitze, Nagel", "to dangle", "baumeln", "upside down", "verkehrt herum", "butt", "Hintern", "to graduate", "den Schulabschluss machen", "understanding", "Verständnis"]
+b = 1
+while True:
+    if b%2==0:
+        print(a[b])
+        b = b + 1
+    else:
+        b= b+1
+        
+    
