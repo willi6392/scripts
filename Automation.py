aae0be64d4f1346578771b91acca875dcf9d13c3
Willi <83806444+JusteWilli@users.noreply.github.com>
2022-10-12 17:06:14 +0200

    Create Automation.py

---
+++ Automation.py
@@ -0,0 +1,8 @@
+# Auto start directory C:\Users\"current User"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+import os 
+import webbrowser
+# Auto open the Browser (Brave Browser)
+
+
+os.startfile('C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe')
+webbrowser.open('https://www.lichess.org/')
